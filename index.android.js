2363cbde5e101563e1624857a7840623e16801e9
aayanqazi <aayanqazi@yahoo.com>
2017-09-15 19:26:46 +0500

    Initial

---
+++ index.android.js
@@ -0,0 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Tourist from "./app/index";
+import {AppRegistry} from 'react-native';
+
+AppRegistry.registerComponent('tourist', () => Tourist);
